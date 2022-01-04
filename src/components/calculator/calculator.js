bfc926ca40b5060b47305c95a9787cdc6c701c26
mahdi <mahdi.moradi.w@gmail.com>
2022-01-04 23:30:31 +0330

    implement logic

--- src/components/calculator/calculator.js
+++ src/components/calculator/calculator.js
@@ -1,14 +1,85 @@
-import React from 'react';
-import Keypad from '../keypad';
-import Screen from '../screen';
-import s from './calculator.module.css';
+import React, { useState } from "react";
+import Keypad from "../keypad";
+import Screen from "../screen";
+import s from "./calculator.module.css";
 
 export default function Calculator() {
-  // TODO: Implement calculator logic here
+  const [digitStack, setDigitStack] = useState([]);
+  const [operand, setOperand] = useState(0);
+  const [operation, setOperation] = useState(null);
+  const [result, setResult] = useState(0);
+
+  const addDigit2Stack = (digit) => {
+    let stack = digitStack;
+    stack.push(digit);
+
+    setDigitStack((prevState) => {return stack});
+    setResult((prevState) => {return digitStack.join("")});
+  };
+
+  const saveOperand = () => {
+    if (digitStack.length != 0) {
+      const newOperand = parseFloat(digitStack.join(""));
+      setDigitStack((prevState) => {return []});
+      setOperand((prevState) => {return newOperand});
+      return newOperand
+    }
+  };
+
+  const saveOperation = (newOperation) => {
+    setOperation((prevState) => {return newOperation});
+    // why this this does not work
+    // setOperation(op)
+  };
+
+  const availableOperations = {
+    "+": (a, b) => {
+      return a + b;
+    },
+    "-": (a, b) => {
+      return a - b;
+    },
+    'x': (a, b) => {
+      return a * b;
+    },
+    "÷": (a, b) => {
+      return a / b;
+    },
+    "%": (a, b) => {
+      return a % b;
+    },
+    cls: () => {
+      setOperand((prevState) => {return 0});
+      setResult((prevState) => {return 0});
+      setOperation((prevState) => {return null});
+    },
+    flip: () => {
+      setOperand((prevState) => {return prevState * -1});
+      setResult((prevState) => {return prevState * -1});
+    },
+    ans: (action, equal=false) => {
+
+      const operand1 = operand;
+      let operand2 = operand;
+      if (digitStack.length != 0) operand2 = saveOperand();
+
+      const res = action(operand1, operand2);
+
+      setResult((prevState) => {return res});
+      setOperand((prevState) => {return res});
+      if (equal)
+        setOperation((prevState) => {return null});
+    },
+  };
+
   return (
     <div className={s.calculator}>
-      <Screen />
-      <Keypad />
+      <Screen text={result} />
+      <Keypad addDigit2Stack={addDigit2Stack}
+              availableOperations={availableOperations}
+              previousOperation={operation}
+              saveOperation={saveOperation}
+              saveOperand={saveOperand}/>
     </div>
   );
 }
--- src/components/keypad/keypad.js
+++ src/components/keypad/keypad.js
@@ -1,52 +1,58 @@
-import React from 'react';
-import s from './keypad.module.css';
-import Button from '../button';
-import cx from 'classnames';
+import React from "react";
+import s from "./keypad.module.css";
+import Button from "../button";
+import cx from "classnames";
 
-export default function Keypad() {
+export default function Keypad({ addDigit2Stack, availableOperations, previousOperation, saveOperation, saveOperand}) {
   const handleButtonClick = (button) => {
-    // TODO: handle clicking here.
-    console.log(button);
-    console.log(
-            button.isAction ? s.dark : s.light
-)
+    if (button.isAction) {
+      switch (button.text) {
+        case "C":
+          availableOperations.cls()
+          break;
+        case "+/-":
+          availableOperations.flip()
+          break;
+        default:
+          saveOperation(availableOperations[button.text])
+          if (previousOperation)
+            availableOperations.ans(previousOperation, button.text == '=' ? true: false);
+          else
+            saveOperand()
+      }
+    } else
+      addDigit2Stack(button.text);
   };
 
   const buttons = [
-    { text: 'C', isAction: true },
-    { text: '+/-', isAction: true },
-    { text: '%', isAction: true },
-    { text: '÷', isAction: true },
-    { text: '7' },
-    { text: '8' },
-    { text: '9' },
-    { text: 'x', isAction: true },
-    { text: '4' },
-    { text: '5' },
-    { text: '6' },
-    { text: '-', isAction: true },
-    { text: '1' },
-    { text: '2' },
-    { text: '3' },
-    { text: '+', isAction: true },
-    { text: '0', isLarge: true },
-    { text: '.' },
-    { text: '=', isAction: true },
+    { text: "C", isAction: true },
+    { text: "+/-", isAction: true },
+    { text: "%", isAction: true },
+    { text: "÷", isAction: true },
+    { text: "7" },
+    { text: "8" },
+    { text: "9" },
+    { text: "x", isAction: true },
+    { text: "4" },
+    { text: "5" },
+    { text: "6" },
+    { text: "-", isAction: true },
+    { text: "1" },
+    { text: "2" },
+    { text: "3" },
+    { text: "+", isAction: true },
+    { text: "0", isLarge: true },
+    { text: "." },
+    { text: "=", isAction: true },
   ];
-  console.log(s.dark)
 
   return (
     <div className={s.keypad}>
       {buttons.map((button) => (
-        <Button
-          key={button.text}
-          text={button.text}
-          onClick={() => handleButtonClick(button)}
-          className={cx(
-            button.isLarge && s['button-2x'],
-            button.isAction ? s.dark : s.light
-          )}
-        />
+        <Button key={button.text}
+                text={button.text}
+                onClick={() => handleButtonClick(button)}
+                className={cx(button.isLarge && s["button-2x"], button.isAction ? s.dark : s.light)}/>
       ))}
     </div>
   );
